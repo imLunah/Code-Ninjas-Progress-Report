60b29083de419278794b534e21878aa58247d62b
imLunah <johndangca@gmail.com>
2026-05-14 11:12:36 -0700

    Add location switcher to mobile bottom nav
    Manager gets a select dropdown; sensei gets a static location label.
    Bumped bottom padding to pb-32 to clear the taller nav.

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
     <div className="min-h-[100dvh] bg-ninja-bg lg:flex">
       <Sidebar />
       <div className="flex-1 flex flex-col min-w-0">
-        <main className="max-w-7xl lg:max-w-none mx-auto w-full px-4 sm:px-6 lg:px-8 py-4 sm:py-8 pb-24 lg:pb-8">
+        <main className="max-w-7xl lg:max-w-none mx-auto w-full px-4 sm:px-6 lg:px-8 py-4 sm:py-8 pb-32 lg:pb-8">
           {children}
         </main>
       </div>
