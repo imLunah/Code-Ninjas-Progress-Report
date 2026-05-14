a41ed8eb4aef2418bf97f122bdd305c7738a6af3
imLunah <johndangca@gmail.com>
2026-05-14 11:10:47 -0700

    Add mobile bottom nav bar and fix layout padding
    Imports MobileNav into Layout so mobile users have navigation. Adds
    pb-24 bottom padding on mobile so content isn't hidden behind the fixed bar.

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,14 +1,16 @@
 import Sidebar from './Sidebar';
+import MobileNav from './MobileNav';
 
 export default function Layout({ children }) {
   return (
     <div className="min-h-[100dvh] bg-ninja-bg lg:flex">
       <Sidebar />
       <div className="flex-1 flex flex-col min-w-0">
-        <main className="max-w-7xl lg:max-w-none mx-auto w-full px-4 sm:px-6 lg:px-8 py-4 sm:py-8">
+        <main className="max-w-7xl lg:max-w-none mx-auto w-full px-4 sm:px-6 lg:px-8 py-4 sm:py-8 pb-24 lg:pb-8">
           {children}
         </main>
       </div>
+      <MobileNav />
     </div>
   );
 }
