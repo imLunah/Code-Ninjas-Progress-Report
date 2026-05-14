2b59142081e829dc69844cd789e3c4317de02dba
imLunah <johndangca@gmail.com>
2026-05-14 16:24:31 -0700

    Rename Senseis nav to Staff; add role-based welcome on dashboards

--- client/src/components/layout/MobileNav.jsx
+++ client/src/components/layout/MobileNav.jsx
@@ -82,7 +82,7 @@ export default function MobileNav() {
     { to: dashPath, label: 'Today', icon: <TodayIcon /> },
     { to: '/manager/students', label: 'Ninjas', icon: <NinjasIcon /> },
     { to: '/clubs', label: 'Clubs', icon: <ClubsIcon /> },
-    { to: '/manager/staff', label: 'Senseis', icon: <SenseiIcon /> },
+    { to: '/manager/staff', label: 'Staff', icon: <SenseiIcon /> },
     { to: '/account', label: 'Account', icon: <AccountIcon profilePicUrl={user.profilePicUrl} initials={initials} /> },
   ];
 
