d95d6fbb8d599726a58bab573e367ccbf83fc419
imLunah <johndangca@gmail.com>
2026-05-21 17:22:40 -0700

    Lower ninja background opacity

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -56,7 +56,7 @@ export default function LandingPage() {
           backgroundImage: 'url(/ninja_background.jpg)',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
-          opacity: 0.18,
+          opacity: 0.08,
         }}
       />
 
