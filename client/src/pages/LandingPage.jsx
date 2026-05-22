fa59f8895d3e9f6dc6a71aa27509a2221b7d65e6
imLunah <johndangca@gmail.com>
2026-05-21 17:48:22 -0700

    Move waving ninja closer to center

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -136,7 +136,7 @@ export default function LandingPage() {
       <motion.img
         src="/ninja_waving.png"
         alt=""
-        className="hidden lg:block absolute right-0 xl:right-8 top-1/2 -translate-y-1/2 h-80 xl:h-96 object-contain pointer-events-none select-none"
+        className="hidden lg:block absolute right-[10%] xl:right-[14%] top-1/2 -translate-y-1/2 h-80 xl:h-96 object-contain pointer-events-none select-none"
         initial={{ opacity: 0, x: 40 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ delay: 0.6, duration: 0.75, ease: [0.16, 1, 0.3, 1] }}
