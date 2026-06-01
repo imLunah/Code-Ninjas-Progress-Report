ac729e28e8a91ca8a6db3d0831e6d9cd6628340d
imLunah <johndangca@gmail.com>
2026-06-01 16:40:35 -0700

    Add 5 new ninja avatar presets; move from programs to profile folder

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -21,11 +21,16 @@ export default function AccountPage() {
   const [avatarError, setAvatarError] = useState('');
 
   const PRESET_AVATARS = [
-    { src: '/profile/ninja-wave.png',    label: 'Wave'    },
-    { src: '/profile/ninja-coder.png',   label: 'Coder'   },
-    { src: '/profile/ninja-coder-2.png', label: 'Coder 2' },
-    { src: '/profile/ninja-gamer.png',   label: 'Gamer'   },
-    { src: '/profile/ninja-hype.png',    label: 'Hype'    },
+    { src: '/profile/ninja-wave.png',       label: 'Wave'       },
+    { src: '/profile/ninja-coder.png',      label: 'Coder'      },
+    { src: '/profile/ninja-coder-2.png',    label: 'Coder 2'    },
+    { src: '/profile/ninja-gamer.png',      label: 'Gamer'      },
+    { src: '/profile/ninja-hype.png',       label: 'Hype'       },
+    { src: '/profile/ninja-kick.png',       label: 'Kick'       },
+    { src: '/profile/ninja-controller.png', label: 'Controller' },
+    { src: '/profile/ninja-run.png',        label: 'Run'        },
+    { src: '/profile/ninja-hacker.png',     label: 'Hacker'     },
+    { src: '/profile/ninja-rocket.png',     label: 'Rocket'     },
   ];
 
   const handlePresetSelect = async (src) => {
