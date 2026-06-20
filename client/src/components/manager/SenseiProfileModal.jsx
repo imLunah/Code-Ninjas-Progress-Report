5d694d8eb1c9a7419a6c1bd444a337694baa77da
imLunah <johndangca@gmail.com>
2026-06-19 21:20:25 -0700

    Remove Edit Login from sensei profile modal
    Senseis now use Reset Login only, matching CD profile.

--- client/src/components/manager/SenseiProfileModal.jsx
+++ client/src/components/manager/SenseiProfileModal.jsx
@@ -138,11 +138,6 @@ export default function SenseiProfileModal({
 
             {showActions && (
               <div className="flex flex-wrap gap-2 mb-5">
-                {!isCD && (
-                  <button className={`${heroBtn} bg-white/10 hover:bg-white/20 text-white`} onClick={() => { handleClose(); onEditLogin(); }}>
-                    Edit Login
-                  </button>
-                )}
                 <button className={`${heroBtn} bg-white/10 hover:bg-white/20 text-white`} onClick={() => { handleClose(); onResetLogin(); }}>
                   Reset Login
                 </button>
